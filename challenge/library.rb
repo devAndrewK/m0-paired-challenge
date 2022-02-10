0d2d584a9b2879a0286677c85d94c496dbb6de44
Andrew King <dev.andrewking@gmail.com>
2022-02-09 21:28:17 -0600

    Add library file, add variables

---
+++ challenge/library.rb
@@ -0,0 +1,21 @@
+# named library because of storing book arrays
+
+# boolean of whether book is checked out or not
+checked_out = false
+
+# int of number of pages
+pages = 0
+
+# string of author
+author = " "
+
+# string of publisher
+publisher = " "
+
+# boolean of whether fiction or non-fiction
+is_fiction = false
+year = 0
+genre = " "
+title = " "
+
+genres = ["Science Fiction", "Non-Fiction", "Fiction", "Crime"]
